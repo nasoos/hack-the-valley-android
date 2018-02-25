1d18752b1001d9089532a701211bb9685b0a3775
t-wu <tw.wu@yahoo.com>
2018-02-25 05:47:57 -0500

    fixed camera, takes photo, is on the screen, but is blurry

--- app/src/main/java/com/example/htvii/hack_the_valley_ii/MakePhotoActivity.java
+++ app/src/main/java/com/example/htvii/hack_the_valley_ii/MakePhotoActivity.java
@@ -1,32 +1,106 @@
 package com.example.htvii.hack_the_valley_ii;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MakePhotoActivity extends BaseActivity {
 
-    Camera camera;
+    /*Camera camera;
     FrameLayout frameLayout;
     ShowCamera showCamera;
 
-    @Override
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+      /*          ".jpg",         /* suffix */
+      /*          storageDir      /* directory */
+     /*   );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void dispatchTakePictureIntent(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+*/
+    ImageView imageView;
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_photo);
-        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+       /* frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
         camera = Camera.open();
         showCamera = new ShowCamera(this,camera);
         frameLayout.addView(showCamera);
+        */
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(resultCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    /*
     Camera.PictureCallback mPictureCallBack = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
@@ -71,5 +145,5 @@ public class MakePhotoActivity extends BaseActivity {
             camera.takePicture(null,null,mPictureCallBack);
         }
 
-    }
+    }*/
 }
