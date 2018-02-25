153d0594f44dcfd8a8134e96d1f53d33015e211c
t-wu <tw.wu@yahoo.com>
2018-02-24 21:49:28 -0500

    camera api with error

---
+++ app/src/main/java/com/example/htvii/hack_the_valley_ii/MakePhotoActivity.java
@@ -0,0 +1,75 @@
+package com.example.htvii.hack_the_valley_ii;
+
+import android.hardware.Camera;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MakePhotoActivity extends AppCompatActivity {
+
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_photo);
+        frameLayout = (FrameLayout)findViewById(R.id.pictureCanvas);
+
+        camera = Camera.open();
+        showCamera = new ShowCamera(this,camera);
+    }
+
+    Camera.PictureCallback mPictureCallBack = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture_file = getOuputMediaFile();
+
+            if (picture_file == null) {
+                return;
+            }
+            else {
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+
+                    camera.startPreview();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private File getOuputMediaFile() {
+        String state = Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED)){
+            return null;
+        }
+        else{
+            File folder_gui = new File(Environment.getExternalStorageDirectory() + File.separator + "GUI");
+            if (!folder_gui.exists()){
+                folder_gui.mkdirs();
+            }
+            File outputFile = new File(folder_gui,"temp.jpg");
+            return outputFile;
+        }
+    }
+
+
+    public void takePicture(View v){
+        if(camera != null){
+            camera.takePicture(null,null,mPictureCallBack);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/htvii/hack_the_valley_ii/ShowCamera.java
@@ -0,0 +1,74 @@
+package com.example.htvii.hack_the_valley_ii;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
+
+
+/**
+ * Created by ton3wu on 2/24/2018.
+ */
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback{
+
+    Camera camera;
+    SurfaceHolder holder;
+
+    public ShowCamera (Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        camera.stopPreview();
+        camera.release();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        Camera.Parameters params = camera.getParameters();
+
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size mSize = null;
+
+        for (camera.Size size: sizes){
+            mSize = size;
+        }
+
+        //orientation
+        if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE){
+            params.set("orientation","portrait");
+            camera.setDisplayOrientation(90);
+            params.setRotation(90);
+        }
+        else {
+            params.set("orientation","landscape");
+            camera.setDisplayOrientation(0);
+            params.setRotation(0);
+        }
+
+        params.setPictureSize(mSize.width, mSize.height);
+        camera.setParameters(params);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
